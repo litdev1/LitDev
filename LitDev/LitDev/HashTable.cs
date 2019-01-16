24809ed98c057dd42ddda4c22740d93ff8076408
steve todman <steve.todman@live.co.uk>
2019-01-16 20:41:01 +0000

    Attribution

--- LitDev/LitDev/HashTable.cs
+++ LitDev/LitDev/HashTable.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿//The following Copyright applies to the LitDev Extension for Small Basic and files in the namespace LitDev.
+//Copyright (C) <2011 - 2015> litdev@hotmail.co.uk
+//This file is part of the LitDev Extension for Small Basic.
+
+//LitDev Extension is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//LitDev Extension is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with LitDev Extension.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using Microsoft.SmallBasic.Library;
 
 namespace LitDev
 {
-    
     /// <summary>
     /// The hashtable provides a fast and efficient way of storing key-value pairs.
     /// Dictionaries have a O(1) scaling and as data size increases the efficiency.
+    /// Code by Abhishek Sathiabalan.
     /// </summary>
     [SmallBasicType]
     public static class LDHashTable
