6aed8d4aae76f7ce419f49fbf6327105688caad0
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-01-16 16:43:25 -0500

    Created Unit Tests Project
    The Unit Testing project will make future changes to an API easier.

---
+++ LitDev Unit Tests/LDHashTable.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LitDev;
+
+
+namespace LitDevTests
+{
+    [TestClass]
+    public class LDHashTableTest
+    {
+        
+        [TestMethod]
+        public void Add()
+        {
+            LDHashtable.Clear("test");
+            int count = LDHashtable.Add("test", "A", 1);
+            Assert.AreEqual(1, count);
+
+            count = LDHashtable.Add("test", "B", 1);
+            Assert.AreEqual(2, count);
+        }
+
+        [TestMethod]
+        public void Remove()
+        {
+            LDHashtable.Clear("test");
+            LDHashtable.Add("test", "A", 1);
+            LDHashtable.Add("test", "B", 1);
+
+            Assert.AreEqual(-1, (int)LDHashtable.Remove("foo","B"));
+            Assert.AreEqual(1, (int)LDHashtable.Remove("test","A"));
+        }
+
+        [TestMethod]
+        public void ContainsKey()
+        {
+            LDHashtable.Clear("test");
+            LDHashtable.Add("test", "A", 1);
+
+            Assert.AreEqual("False", LDHashtable.ContainsKey("foo12","A").ToString());
+            Assert.AreEqual("False", LDHashtable.ContainsKey("test", "B").ToString());
+            Assert.AreEqual("True", LDHashtable.ContainsKey("test", "A").ToString());
+        }
+
+        [TestMethod]
+        public void ContainsValue()
+        {
+            LDHashtable.Clear("test");
+            LDHashtable.Add("test", "A", 1);
+            LDHashtable.Add("test", "B", 2);
+
+            Assert.AreEqual("True", LDHashtable.ContainsValue("test", 1).ToString());
+            Assert.AreEqual("False", LDHashtable.ContainsValue("test", 3).ToString());
+            Assert.AreEqual("False", LDHashtable.ContainsValue("fbs1",3).ToString());
+        }
+
+        [TestMethod]
+        public void Clear()
+        {
+            LDHashtable.Clear("test");
+            LDHashtable.Add("test", "A", 1);
+            LDHashtable.Add("test", "B", 2);
+
+            Assert.AreEqual(-1, (int)LDHashtable.Clear("sjasf"));
+            Assert.AreEqual(0, (int)LDHashtable.Clear("test"));
+        }
+
+        [TestMethod]
+        public void GetValue()
+        {
+            LDHashtable.Clear("test");
+            LDHashtable.Add("test", "A", 1);
+            LDHashtable.Add("test", "B", 2);
+
+            Assert.AreEqual("", LDHashtable.GetValue("test", "C").ToString() );
+            Assert.AreEqual("", LDHashtable.GetValue("fo334", "C").ToString());
+            Assert.AreEqual(1, (int)LDHashtable.GetValue("test","A"));
+        }
+    }
+}
---
+++ LitDev Unit Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LitDev Unit Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LitDev Unit Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e6509e8-5a1b-4019-81ad-26d67af80df7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
