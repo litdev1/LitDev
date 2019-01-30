dbd34d27629b83a764047b66bff0b5df348d2797
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-01-30 15:07:55 -0500

    Added test coverage for LDText.Split

---
+++ LitDevUnitTests/LDText.cs
@@ -0,0 +1,24 @@
+﻿using LitDev;
+using Microsoft.SmallBasic.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LitDevUnitTests
+{
+    [TestClass]
+    public class LDTextTest
+    {
+        [TestMethod]
+        public void Split()
+        {
+            string phrase = "The quick brown fox jumps over the lazy dog, twice a month";
+            Primitive array = "1=The;2=quick;3=brown;4=fox;5=jumps;6=over;7=the;8=lazy;9=dog,;" +
+                              "10=twice;11=a;12=month;";
+
+            Primitive array2 = "1=The;2=quick;3=brown;4=fox;5=jumps;6=over;7=the;8=lazy;9=dog;" +
+                               "10=twice;11=a;12=month;";
+
+            Assert.AreEqual( array, LDText.Split(phrase," ") );
+            Assert.AreEqual( array2, LDText.Split(phrase, "1= ;2=,;"));
+        }
+    }
+}
